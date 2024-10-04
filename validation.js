9bfc8b062494cbfae079a42f78b5d7cade9a9939
sramirez124 <forwardemails124@gmail.com>
2024-10-03 23:31:39 -0500

    password check works, just need to make it go to homepage

--- validation.js
+++ validation.js
@@ -1,12 +1,13 @@
-const passWord = document.getElementById("inputPassword4").value;
-const confirmPassword = document.getElementById("passwordConfirm").value;
+
 
 function validateForm(){
-    const passwordRequirements = /^(?=.[a-z])(?=.[A-Z])(?=.[0-9]){6,12}$/;
+    const passWord = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("passwordConfirm").value;
+    const passwordRequirements = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{6,12}$/;
     if (passwordRequirements.test(passWord) == false){
         console.log(passWord);
-        console.log(passwordRequirements.test(passWord))
-        window.confirm("Password Requirements not met: \n 1. At least one lowercase \n 2. At least one uppercase \n 3. At least one Numeric digit \n 4. Must have a length of 6 to 12 characters")
+        console.log(passwordRequirements.test(passWord));
+        window.confirm("Password Requirements not met: \n 1. At least one lowercase \n 2. At least one uppercase \n 3. At least one Numeric digit \n 4. At least one special character \n 5. Must have a length of 6 to 12 characters")
     }
 
     if (passWord != confirmPassword){
